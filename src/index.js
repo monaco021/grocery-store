6829c98cc75a0cf26e72664e6ea3f46ce4434b8c
james <monaco.james18@gmail.com>
2020-12-15 12:19:59 -0600

    index.js setup and test complete

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+import configureStore from "./store/index";
+import { Provider } from "react-redux";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
 function Root() {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
